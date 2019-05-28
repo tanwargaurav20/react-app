b616eb715d67ec17e8032273ef4e5e134a5b53f9
GT <tanwar.gaurav.20@gmail.com>
2019-05-28 16:18:44 +0100

    Updates

---
+++ precache-manifest.88dab11da6d42729822ec813135bf854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e388f026f85d3b15adbd30f1c5bff11f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d9043189f4d05e5eb37a",
+    "url": "/react-app/static/css/main.f612f6a5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d9043189f4d05e5eb37a",
+    "url": "/react-app/static/js/main.adc7da13.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
